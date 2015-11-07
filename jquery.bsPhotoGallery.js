6bf65c00eab9dbfc50e799a9589c842a1d5cce62
mikesoriano <michael.soriano@live.com>
2015-11-07 09:51:45 -0800

    added createModal to plugin

--- jquery.bsPhotoGallery.js
+++ jquery.bsPhotoGallery.js
@@ -1,6 +1,20 @@
 (function($) {
-  "use strict"; 
-  $.fn.bsPhotoGallery = function() {
+  "use strict";
+  $.fn.bsPhotoGallery = function(options) {
+    
+      function createModalHTML(){
+        var modal = '';
+        modal += '<div class="modal fade" id="myModal" tabindex="-1" role="dialog"';
+        modal += 'aria-labelledby="myModalLabel" aria-hidden="true">';
+        modal += '<div class="modal-dialog"><div class="modal-content">';
+        modal += '<div class="modal-body"></div></div></div></div>';
+        $('body').append(modal);
+      }
+
+
+       createModalHTML();
+
+
       return this;
-  }; 
+  };
 }(jQuery));
