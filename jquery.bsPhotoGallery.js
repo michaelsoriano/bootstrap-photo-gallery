bc55914b912998d8bfdc35a8b29a0fc1585df3bc
mikesoriano <michael.soriano@live.com>
2015-11-08 08:06:33 -0800

    Moved modal code into plugin. Removed bootstrap classes fr demo

--- jquery.bsPhotoGallery.js
+++ jquery.bsPhotoGallery.js
@@ -1,18 +1,134 @@
 (function($) {
   "use strict";
   $.fn.bsPhotoGallery = function(options) {
-    
-      function createModalHTML(){
+
+    // window.bspNameSpace = window.bspNameSpace || {};
+    //class="col-lg-2 col-md-2 col-sm-3 col-xs-4 col-xxs-12"
+
+      var settings = $.extend({
+          // classPrefix: "sr",
+          // firstRowHeader : true,
+          // showLabels : true,
+          // altRowStyle : true
+      }, options);
+
+      var id = generateId();
+
+      this.each(function(i){
+        // console.log(id + '-' +i);
+        var items = $(this).find('li');
+        $(this).attr('data-bsp-ul-id', id);
+        $(this).attr('data-bsp-ul-index', i);
+        //set up each item in ul
+        items.each(function(x){
+          // console.log(id + '-' +i);
+          $(this).attr('data-bsp-li-index', x).on('click', showModal);
+        });
+      })
+
+      var clicked = {};
+      // console.log(this.length);
+
+      function getCurrentUl(){
+        return 'ul[data-bsp-ul-id="'+clicked.ulId+'"][data-bsp-ul-index="'+clicked.ulIndex+'"]';
+      }
+      function generateId() {
+        var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var ID_LENGTH = 4;
+        var out = '';
+        for (var i = 0; i < ID_LENGTH; i++) {
+          out += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+        }
+        return 'bsp-'+out;
+      }
+      function createModalWrap(){
+
+        if($('#bsPhotoGalleryModal').length !== 0){
+          return false;
+        }
+
         var modal = '';
-        modal += '<div class="modal fade" id="myModal" tabindex="-1" role="dialog"';
+        modal += '<div class="modal fade" id="bsPhotoGalleryModal" tabindex="-1" role="dialog"';
         modal += 'aria-labelledby="myModalLabel" aria-hidden="true">';
         modal += '<div class="modal-dialog"><div class="modal-content">';
         modal += '<div class="modal-body"></div></div></div></div>';
         $('body').append(modal);
+
+      }
+      function showHideControls(){
+    		var total = $(getCurrentUl()+' li[data-bsp-li-index]').length;
+
+    		if(total === clicked.nextImg){
+    			$('a.next').hide();
+    		}else{
+    			$('a.next').show()
+    		}
+    		if(clicked.prevImg === -1){
+    			$('a.previous').hide();
+    		}else{
+    			$('a.previous').show()
+    		}
+    	}
+      function showModal(){
+
+          var src = $(this).find('img').attr('src');
+          var index = $(this).attr('data-bsp-li-index');
+          var ulIndex = $(this).parent('ul').attr('data-bsp-ul-index');
+          var ulId = $(this).parent('ul').attr('data-bsp-ul-id');
+
+          clicked.img = src;
+          clicked.prevImg = parseInt(index) - parseInt(1);
+      		clicked.nextImg = parseInt(index) + parseInt(1);
+          clicked.ulIndex = ulIndex;
+          clicked.ulId = ulId;
+
+
+          $('#bsPhotoGalleryModal').modal();
+
+          var html = '';
+          var img = '<img src="' + clicked.img + '" class="img-responsive"/>';
+
+          html += img;
+          html += '<div style="height:25px;clear:both;display:block;">';
+          html += '<a class="controls next" data-bsp-id="'+clicked.ulId+'" href="'+ (clicked.nextImg) + '">next &raquo;</a>';
+          html += '<a class="controls previous" data-bsp-id="'+clicked.ulId+'" href="' + (clicked.prevImg) + '">&laquo; prev</a>';
+          html += '</div>';
+          $('#bsPhotoGalleryModal .modal-body').html(html);
+          showHideControls();
+      }
+      function nextPrevHandler(){
+
+          var ul = $(getCurrentUl());
+          var index = $(this).attr('href');
+          var src = ul.find('li[data-bsp-li-index="'+index+'"] img').attr('src');
+
+          $('.modal-body img').attr('src', src);
+
+          clicked.prevImg = parseInt(index) - 1;
+          clicked.nextImg = parseInt(clicked.prevImg) + 2;
+
+          if($(this).hasClass('previous')){
+              $(this).attr('href', clicked.prevImg);
+              $('a.next').attr('href', clicked.nextImg);
+          }else{
+              $(this).attr('href', clicked.nextImg);
+              $('a.previous').attr('href', clicked.prevImg);
+          }
+          // console.log(clicked);
+        showHideControls();
+        return false;
+      }
+      function clearModalContent(){
+        $('#bsPhotoGalleryModal .modal-body').html('');
+        clicked = {};
       }
 
+      //this is for the next / previous buttons
+      $(document).on('click', 'a.controls[data-bsp-id="'+id+'"]', nextPrevHandler);
+      $(document).on('hidden.bs.modal', '#bsPhotoGalleryModal', clearModalContent);
 
-       createModalHTML();
+      //start init methods
+      createModalWrap();
 
 
       return this;
